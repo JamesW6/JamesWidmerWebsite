43852ff60baaae835fe224d0527db26e669d99fd
James Widmer <jow257@pitt.edu>
2026-02-03 17:19:03 -0500

    adds css file with basic styling

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Happy developing ✨')
+function listenerFunction(this: HTMLElement, event: Event) {
+    alert("i hate you");
+    console.log("Button clicked!");
+}
+export function initFunction() {
+    console.log("i hate you");
+    const element = document.getElementById('mainButton');
+    element?.addEventListener('click', listenerFunction);
+}
+
+initFunction();
